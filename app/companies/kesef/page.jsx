6a5d52c2ec848ab7c1da4e609295309190c86676
liron93 <96621915+liron93@users.noreply.github.com>
2026-09-14 17:23:10 +0300

    style(kesef): use fullWidth CompanyShell layout
    Approved by Liron in chat 2026-09-14.

--- app/companies/kesef/page.jsx
+++ app/companies/kesef/page.jsx
@@ -2,5 +2,5 @@ import CompanyShell from "../CompanyShell";
 import Company from "@/companies/kesef/Company";
 
 export default function Page() {
-  return <CompanyShell title="כלכלה והשקעות"><Company /></CompanyShell>;
+  return <CompanyShell title="כלכלה והשקעות" fullWidth><Company /></CompanyShell>;
 }
